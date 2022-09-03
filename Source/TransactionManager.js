08169c8372b831c3cb15c7774e016d15d536a430
Nicolas <nicolascari.rz@gmail.com>
2022-09-03 19:15:22 -0400

    se puede aumentar ingresos y egresos

--- Source/TransactionManager.js
+++ Source/TransactionManager.js
@@ -1,6 +1,31 @@
-function getMonto()
+function getMonto(bloqueMonto)
 {
-    return 0
+    bloqueMonto.innerHTML= 0
 }
 
-export {getMonto}
+
+function updateMonto(transaccion, bloqueMonto)
+{
+    let montoActual = Number.parseFloat(bloqueMonto.innerHTML);
+    if (transaccion["tipo"] == "ingreso")
+    {
+        montoActual += transaccion["monto"]
+    }
+    else
+    {
+        montoActual -= transaccion["monto"]
+    }
+    bloqueMonto.innerHTML = montoActual;
+}
+
+function addTransaction(monto, tipo, bloqueMonto)
+{
+    let dict = {'tipo':tipo,'monto':monto}   
+    updateMonto(dict, bloqueMonto)
+}
+
+
+
+
+
+export {getMonto, addTransaction}
--- Source/presenter.js
+++ Source/presenter.js
@@ -1,10 +1,32 @@
-import { getMonto } from "./TransactionManager.js"
+import {getMonto, addTransaction} from "./TransactionManager.js"
 
+const ingreso = document.querySelector("#ingreso");
+const egreso = document.querySelector("#egreso");
 
+const transaccion = document.querySelector("#transaccion");
 const CampoMonto = document.getElementById("MontoField");
+
+
+ingreso.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let dinero = Number.parseInt(transaccion.value)
+    addTransaction(dinero,"ingreso",CampoMonto)
+
+});
+
+egreso.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let dinero = Number.parseFloat(transaccion.value)
+    addTransaction(dinero,"egreso",CampoMonto)
+
+
+});
+
 function LoadFunction()
 {
-    CampoMonto.innerHTML=getMonto()
+    getMonto(CampoMonto)
 }
 
 document.onload=LoadFunction()
